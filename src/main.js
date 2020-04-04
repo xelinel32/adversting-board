4817e56b0987975307b48167da3f4c9285fbd627
xelinel32 <artemsedlyar99@gmail.com>
2020-04-04 16:32:06 +0300

    include icon obj to constructor Vutify

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: "mdi"
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
+  vuetify: new Vuetify(),
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
