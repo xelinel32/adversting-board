e958575642ac503d5c34137058ab7009eedcbaf8
xelinel32 <artemsedlyar99@gmail.com>
2020-04-04 16:31:04 +0300

    set up routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,52 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import Ad from "@/components/Ads/Ad";
+import AdList from "@/components/Ads/AdList";
+import NewAd from "@/components/Ads/NewAd";
+import Login from "@/components/Auth/Login";
+import Register from "@/components/Auth/Register";
+import Orders from "@/components/User/Orders";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: []
-})
+  routes: [
+    {
+      path: "",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/ad/:id",
+      name: "ad",
+      component: Ad
+    },
+    {
+      path: "/list",
+      name: "list",
+      component: AdList
+    },
+    {
+      path: "/new",
+      name: "newAd",
+      component: NewAd
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "reg",
+      component: Register
+    },
+    {
+      path: "/orders",
+      name: "orders",
+      component: Orders
+    }
+  ],
+  mode: "history"
+});
