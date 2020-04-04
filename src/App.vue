4047d705ff8c3baadbcc25462f635958d1f1c6c0
xelinel32 <artemsedlyar99@gmail.com>
2020-04-04 16:32:49 +0300

    create app template home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,78 @@
 <template>
-  <h1>Test</h1>
+  <v-app>
+    <v-navigation-drawer app dark temporary v-model="drawer">
+      <v-list>
+        <v-list-item v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark color="orange">
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Adversting board</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+          text
+          color="normal"
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}</v-btn
+        >
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {
+          title: "Login",
+          icon: "mdi-lock",
+          url: "/login"
+        },
+        {
+          title: "Register",
+          icon: "mdi-face",
+          url: "/register"
+        },
+        {
+          title: "Orders",
+          icon: "mdi-bookmark",
+          url: "/orders"
+        },
+        {
+          title: "New ad",
+          icon: "mdi-note",
+          url: "/new"
+        },
+        {
+          title: "My ads",
+          icon: "mdi-format-list-text",
+          url: "/list"
+        }
+      ]
+    };
+  }
+};
 </script>
