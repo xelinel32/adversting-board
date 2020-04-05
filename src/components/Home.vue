62e7e904993b9a769a7033fa3b0d39c01e7fa2a0
xelinel32 <artemsedlyar99@gmail.com>
2020-04-05 15:59:12 +0300

    create router to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,73 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex md12>
-        <h1>Home</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex md12>
+          <v-carousel>
+            <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.imageSrc">
+            </v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+      <v-layout row>
+        <v-flex md12 v-for="ad in ads" :key="ad.id">
+          <v-card>
+            <v-img :src="ad.imageSrc" height="200px ">
+              <v-card-title primary-title
+                ><div>
+                  <h3 class="headline md-0">{{ ad.title }}</h3>
+                  <div>{{ ad.description }}</div>
+                </div></v-card-title
+              >
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat text>
+                Open
+              </v-btn>
+
+              <v-btn raised dark class="primary" text>
+                Buy
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      ads: [
+        {
+          title: "First",
+          description: "Hello i'm description",
+          promo: false,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "1223"
+        },
+        {
+          title: "Second ad",
+          description: "Hello i'm description",
+          promo: false,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "12123"
+        },
+        {
+          title: "Thirt ad",
+          description: "Hello i'm description",
+          promo: false,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "746"
+        }
+      ]
+    };
   }
 };
 </script>
