4a7bb4a0f14dd19e77146acabf8d9d8262fe4ea0
xelinel32 <artemsedlyar99@gmail.com>
2020-04-06 16:42:48 +0300

    create template for Orders list

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,26 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex md12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary" mb-5>Orders</h1>
+        <v-list subheader two-line elevation="6">
+          <v-list-item v-for="order in orders" :key="order.id">
+            <v-list-item-action>
+              <v-checkbox
+                color="success"
+                :input-value="order.done"
+                @change="markDone(order)"
+              ></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ order.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn :to="'/ad/' + order.adId" dark color="orange"> Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,9 +29,24 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      orders: [
+        {
+          id: "2132",
+          name: "Artemy",
+          phone: "380935039351",
+          adId: "123",
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    markDone() {
+      order.done = true;
+    }
   }
 };
 </script>
 
-<style></style>
+<style scoped></style>
