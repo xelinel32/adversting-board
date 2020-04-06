954e1961f172bee9e79c1e7e1a2631db96a2c0c8
xelinel32 <artemsedlyar99@gmail.com>
2020-04-06 16:43:50 +0300

    redesign Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,33 +5,33 @@
         <v-flex md12>
           <v-carousel>
             <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.imageSrc">
+              <div class="carousel__link">
+                <v-btn large color="error" :to="'/ad/' + ad.id"
+                  >{{ ad.title }}
+                </v-btn>
+              </div>
             </v-carousel-item>
           </v-carousel>
         </v-flex>
       </v-layout>
     </v-container>
-    <v-container>
-      <v-layout row>
-        <v-flex md12 v-for="ad in ads" :key="ad.id">
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 v-for="ad in ads" :key="ad.id">
           <v-card>
-            <v-img :src="ad.imageSrc" height="200px ">
-              <v-card-title primary-title
-                ><div>
-                  <h3 class="headline md-0">{{ ad.title }}</h3>
-                  <div>{{ ad.description }}</div>
-                </div></v-card-title
-              >
-            </v-img>
-
+            <v-img :src="ad.imageSrc" height="200px"> </v-img>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ ad.title }}</h3>
+                <div>
+                  {{ ad.description }}
+                </div>
+              </div>
+            </v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn flat text>
-                Open
-              </v-btn>
-
-              <v-btn raised dark class="primary" text>
-                Buy
-              </v-btn>
+              <v-btn :to="'/ad/' + ad.id">Open</v-btn>
+              <v-btn dark color="orange">Buy</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -72,4 +72,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.carousel__link {
+  position: absolute;
+  bottom: 100px;
+  left: 50%;
+  background: rgba(0, 0, 0, 0.3);
+  transform: translate(-50%, 0);
+  padding: 10px 15px;
+  border-radius: 5px;
+}
+</style>
