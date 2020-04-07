4a9382af8d98962ae4b0695f1102c6dd2ef4e774
xelinel32 <artemsedlyar99@gmail.com>
2020-04-07 15:18:16 +0300

    initialize firebase in life cycle app - created()

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App";
 import router from "./router";
 import store from "./store";
 import Vuetify from "vuetify";
+import * as fb from "firebase";
 import "vuetify/dist/vuetify.min.css";
 import "@mdi/font/css/materialdesignicons.css";
 
@@ -22,5 +23,16 @@ new Vue({
   store,
   vuetify: new Vuetify(),
   components: { App },
-  template: "<App/>"
+  template: "<App/>",
+  created() {
+    // Initialize Firebase
+    fb.initializeApp({
+      apiKey: "AIzaSyCgDOoKKoM9ZF7bsb47VeaTb8-v3ZzBbRg",
+      authDomain: "ad-board-5c691.firebaseapp.com",
+      databaseURL: "https://ad-board-5c691.firebaseio.com",
+      projectId: "ad-board-5c691",
+      storageBucket: "ad-board-5c691.appspot.com",
+      messagingSenderId: "654246470341"
+    });
+  }
 });
