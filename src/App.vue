f0b56892f1e70a20ec0ef5920af4f64ea776b3a2
xelinel32 <artemsedlyar99@gmail.com>
2020-04-09 13:46:25 +0300

    create logiut method

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,14 @@
             <v-list-item-title v-text="link.title"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <v-list-item @click="onLogout" v-if="isUserLoggedIn">
+          <v-list-item-icon>
+            <v-icon left>mdi-exit-to-app</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="'Logout'"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
@@ -33,8 +41,12 @@
           color="normal"
         >
           <v-icon left>{{ link.icon }}</v-icon>
-          {{ link.title }}</v-btn
-        >
+          {{ link.title }}
+        </v-btn>
+        <v-btn @click="onLogout" text color="normal" v-if="isUserLoggedIn">
+          <v-icon left>mdi-exit-to-app</v-icon>
+          Logout
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
@@ -62,44 +74,58 @@
 export default {
   data() {
     return {
-      drawer: false,
-      links: [
-        {
-          title: "Login",
-          icon: "mdi-lock",
-          url: "/login"
-        },
-        {
-          title: "Register",
-          icon: "mdi-face",
-          url: "/register"
-        },
-        {
-          title: "Orders",
-          icon: "mdi-bookmark",
-          url: "/orders"
-        },
-        {
-          title: "New ad",
-          icon: "mdi-note",
-          url: "/new"
-        },
-        {
-          title: "My ads",
-          icon: "mdi-format-list-text",
-          url: "/list"
-        }
-      ]
+      drawer: false
     };
   },
   computed: {
     error() {
       return this.$store.getters.error;
+    },
+    isUserLoggedIn() {
+      return this.$store.getters.isUserLoggedIn;
+    },
+    links() {
+      if (this.isUserLoggedIn) {
+        return [
+          {
+            title: "Orders",
+            icon: "mdi-bookmark",
+            url: "/orders"
+          },
+          {
+            title: "New ad",
+            icon: "mdi-note",
+            url: "/new"
+          },
+          {
+            title: "My ads",
+            icon: "mdi-format-list-text",
+            url: "/list"
+          }
+        ];
+      } else {
+        return [
+          {
+            title: "Login",
+            icon: "mdi-lock",
+            url: "/login"
+          },
+          {
+            title: "Register",
+            icon: "mdi-face",
+            url: "/register"
+          }
+        ];
+      }
     }
   },
   methods: {
     closeError() {
       this.$store.dispatch("clearError");
+    },
+    onLogout() {
+      this.$store.dispatch("logoutUser");
+      this.$router.push("/");
     }
   }
 };
