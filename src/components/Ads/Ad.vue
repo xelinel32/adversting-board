808b21e2f9f636517693fd566c4cebcbcdfd5d1a
xelinel32 <artemsedlyar99@gmail.com>
2020-04-10 16:45:39 +0300

    registration component and bound data

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <v-card>
+        <v-card v-if="!loading">
           <v-img :src="ad.imageSrc" height="300px"> </v-img>
           <v-card-text>
             <h1 class="text--primary mb-5">{{ ad.title }}</h1>
@@ -12,23 +12,38 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="primary" large tile text>Edit</v-btn>
+            <addEditAdModal :ad="ad"></addEditAdModal>
             <v-btn class="success" large>Buy</v-btn>
           </v-card-actions>
         </v-card>
+        <div v-else class="text-center">
+          <v-progress-circular
+            :size="100"
+            :width="4"
+            color="orange"
+            indeterminate
+          ></v-progress-circular>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import EditAdModalVue from "./EditAdModal.vue";
 export default {
   props: ["id"],
   computed: {
     ad() {
       const id = this.id;
       return this.$store.getters.adById(id);
+    },
+    loading() {
+      return this.$store.getters.loading;
     }
+  },
+  components: {
+    addEditAdModal: EditAdModalVue
   }
 };
 </script>
