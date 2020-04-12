08791890af7bd33eb9cbd760a1240941edb16ed3
xelinel32 <artemsedlyar99@gmail.com>
2020-04-12 13:56:31 +0300

    include component app-buy-modal

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,7 +35,7 @@
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn :to="'/ad/' + ad.id">Open</v-btn>
-              <v-btn dark color="orange">Buy</v-btn>
+              <app-buy-modal :ad="ad"></app-buy-modal>
             </v-card-actions>
           </v-card>
         </v-flex>
